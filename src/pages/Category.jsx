56d35acb396b217a2db4f1cdf2c9c85485468a8f
ToniGrbic <tonigrbic.5@gmail.com>
2022-10-22 15:35:08 +0200

    finished Category page

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,10 +1,71 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-const Category = () => {
-  const { category }= useParams()
-  return (
-    <div>{category}</div>
-  )
-}
+import React from "react";
+import { useParams, Link } from "react-router-dom";
+import SingleItemMainCarousel from "../components/carousels/SingleItemMainCarousel";
+import Sidebar from "../components/sections/Sidebar";
+import Banner from "../components/Banner";
+import '../styles/CategoryPageContent.scss'
+const articles = Array(5).join(".").split(".");
+const pagesSelect = Array.from(Array(10).keys());
 
-export default Category
+const Category = () => {
+  const { category } = useParams();
+  return (
+    <>
+      <SingleItemMainCarousel />
+      <div className="mainContent">
+        <div className="categoryArticles">
+          <h2>{category}</h2>
+          {articles?.map((article, index) => {
+            return (
+              <div className="categoryArticle" key={index}>
+                <div className="articleTitle">
+                  <h3>
+                    Patriotsvv make cuts ... and Tim Tebow survives (so far)
+                  </h3>
+                </div>
+                <div className="dateAndAuthor">
+                  <p>
+                    <img src="calendar.png" />
+                    August 26, 2013
+                  </p>
+                  <p>Author: Matej Sudar</p>
+                  <p>12 Comments</p>
+                </div>
+                <div className="articleContent">
+                  <img src="/Layer 57.png" />
+                  <div className="articleText">
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                      sed diam nonummy nibh euismod tincidunt ut laoreet dolore
+                      magna aliquam erat volutpat. Ut wisi enim ad minim veniam,
+                      quis nostrud exerci tation ullamcorper suscipit lobortis
+                      nisl ut aliquip ex ea commodo consequat. Ut wisi enim ad
+                      minim veniam, quis nostrud exerci tation ullamcorper
+                      suscipit lobortis nisl ut aliquip ex ea commodo.
+                    </p>
+                    <button className="readArticleBtn purpleBtn">
+                      <Link to={`/articles/${index}`}>Read Article</Link>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+          <div className="pageCountDiv">
+            {pagesSelect?.map((pageSelect) => {
+              return (
+                <div className="pageSelectDiv" key={pageSelect}>
+                  <p>{pageSelect + 1}</p>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <Sidebar />
+      </div>
+      <Banner width={620} height={120} />
+    </>
+  );
+};
+
+export default Category;
