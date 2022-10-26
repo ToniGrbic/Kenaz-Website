52ff80d7ef5be196d97f1ad21065ce089e0016bd
ToniGrbic <tonigrbic.5@gmail.com>
2022-10-26 22:44:20 +0200

    added scroll to top on page transition

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import Footer from "../components/sections/Footer";
 import Header from "../components/sections/Header";
 import Banner from "../components/Banner";
+import ScrollToTop from "./ScrollToTop";
+
 const Layout = (props) => {
   return (
     <div className="pageLayout">
+      <ScrollToTop/>
       <Header />
       <main className="contentWrapper">
         <Banner width={940} height={120} />
